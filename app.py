72a49cb46e1503d82885442de15abfc99b596795
Kannan <kamalakannan1011@gmail.com>
2020-06-15 22:30:05 -0400

    Created logic to add the table list using pandas in util.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import sys
 from config import DB_DETAILS
-
+from util import get_tables
 def main():
     """Pass one argument"""
     env = sys.argv[1]
     db_details = DB_DETAILS[env]
-    print(db_details)
+    tables = get_tables('table_list')
+    for table in tables['table_name']:
+        print(table)
+        # print(idx)
 if __name__ == '__main__':
     main()
