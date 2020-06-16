02f2f010dbf68bc222f36804f6c12f72a714c1bb
Kannan <kamalakannan1011@gmail.com>
2020-06-15 21:11:37 -0400

    Intial commit 1

---
+++ app.py
@@ -0,0 +1,10 @@
+import sys
+from config import DB_DETAILS
+
+def main():
+    """Pass one argument"""
+    env = sys.argv[1]
+    db_details = DB_DETAILS[env]
+    print(db_details)
+if __name__ == '__main__':
+    main()
